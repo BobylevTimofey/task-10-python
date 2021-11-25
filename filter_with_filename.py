38284851e0ba39698132ab07785b5a5cb0e5f6f6
BobylevTimofey <bobylevvg@gmail.com>
2021-11-25 17:04:19 +0500

    add filter_with_filename.py

---
+++ filter_with_filename.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import numpy as np
+
+
+def apply_filter():
+    img = Image.open("cat test photo.jpg")
+    mosaic_size = 10
+    count_gradations = 5
+    data_img = np.array(img)
+    size_img = data_img.shape
+
+    for x in range(0, size_img[0], mosaic_size):
+        for y in range(0, size_img[1], mosaic_size):
+            paint_cell(data_img, x, y, mosaic_size, count_gradations)
+
+    res = Image.fromarray(data_img)
+    res.save('res.jpg')
+
+
+def find_average_brightness_cell(arr, px_width, px_height, mosaic_size):
+    return np.average(arr[px_width:px_width + mosaic_size, px_height: px_height + mosaic_size])
+
+
+def paint_cell(arr, px_width, px_height, mosaic_size, count_gradations):
+    average_brightness = find_average_brightness_cell(arr, px_width, px_height, mosaic_size)
+    gradations = 255 // count_gradations
+    px_brightness = average_brightness // gradations * gradations
+    arr[px_width:px_width + mosaic_size, px_height:px_height + mosaic_size, ][:] = px_brightness
+
+
+if __name__ == "__main__":
+    apply_filter()
